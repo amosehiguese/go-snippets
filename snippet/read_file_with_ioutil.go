48424969b11e20f3e29266acd683d1421726480f
amosehiguese <amosehiguese@gmail.com>
2023-04-23 22:33:37 +0100

    Add read file snippet

---
+++ snippet/read_file_with_ioutil.go
@@ -0,0 +1,28 @@
+package snippet
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func ReadFileWithIoutil() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "err: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+
+	for line, count := range counts {
+		if count > 1 {
+			fmt.Printf("%d\t%s\n", count, line)
+		}
+	}
+}
