2b84eac37d3315d050e5175cc9f9766b805a2ddf
amosehiguese <amosehiguese@gmail.com>
2023-05-02 21:04:25 +0100

    Add printjson.go

---
+++ snippet/printjson.go
@@ -0,0 +1,27 @@
+package snippet
+
+import "fmt"
+
+func PrintJSON(v interface{}) {
+	switch vv := v.(type) {
+	case string:
+		fmt.Println("is string", vv)
+	case float64:
+		fmt.Println("is float64", vv)
+	case []interface{}:
+		fmt.Println("is an array:")
+		for i, u := range vv {
+			fmt.Print(i, " ")
+			PrintJSON(u)
+		}
+	case map[string]interface{}:
+		fmt.Println("is an object:")
+		for i, u := range vv {
+			fmt.Print(i, " ")
+			PrintJSON(u)
+		}
+	default:
+		fmt.Println("Unknown type")
+	}
+
+}
